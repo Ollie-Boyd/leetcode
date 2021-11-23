396fe822769149e36f71447d7cae7fbcc4f89a89
Ollie-Boyd <ollyboyd@hotmail.co.uk>
2021-11-23 19:31:18 +0000

    finished 448

--- 448.find-all-numbers-disappeared-in-an-array.py
+++ 448.find-all-numbers-disappeared-in-an-array.py
@@ -5,8 +5,15 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        
+        length = len(nums)
+        print(length)
+        comparison_list = list(range(1, length+1))
+        print(comparison_list)
+        return list(set(comparison_list) - set(nums))
+
 # @lc code=end
 
